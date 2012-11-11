77fa92249752bbef8ba0615a8202c3e1614e9667
Frank Fang <frankfang1990@gmail.com>
2012-11-11 14:54:00 +0800

    2 new exc

---
+++ exec_and_scope.py
@@ -0,0 +1,5 @@
+from math import sqrt
+scope = {}
+exec 'print 1' in scope
+print (eval("1+4"))
+print(sqrt(4))
---
+++ external.py
@@ -0,0 +1,6 @@
+def conbine ( parameter ):
+	external = 'I donnot wanna this'
+	print parameter , globals()['external']
+
+external = 'berry'
+conbine('Shrub')
