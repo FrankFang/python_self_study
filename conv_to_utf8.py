dc3c63d58e549e3782446d88c22228bd68c26fc9
Frank Fang <frankfang1990@gmail.com>
2012-11-23 23:53:50 +0800

    fix bugs. beautify code.

--- conv_to_utf8.py
+++ conv_to_utf8.py
@@ -10,21 +10,25 @@ def convertToUtf8(srcDir,desDir):
   suffixiList = ('.shtml','.html','.css','.js')
   dirDict = os.walk(srcDir)
   for root,dirs,files in dirDict:
-    for aFilePath in files if aFilePath.endswith(): 
+    for aFilePath in files: 
+      if not aFilePath.endswith(suffixiList):
+        continue
+      print('to convert: '+aFilePath);
       oldPath =  os.path.join(root,aFilePath)
       newPath = oldPath.replace(srcDir,desDir)
-      tempPath = newPath+'.temp'
       newDir = root.replace(srcDir,desDir)
       if not os.path.exists(newDir):
         os.mkdir(newDir)
 
-      shutil.copyfile(oldPath,tempPath)
-      tempFile = codecs.open(tempPath)
-      content = tempFile.read()
-      tempFile.close()
-      os.remove(tempPath)
+      //获取旧文件的内容
+      oldFile = codecs.open(oldPath)
+      content = oldFile.read()
+      oldFile.close()
+
+      //猜测旧的文件编码格式
       oldEncoding = chardet.detect(content)['encoding']
       content = content.decode(oldEncoding)
+
       codecs.open(newPath,'w',encoding='utf8').write(content)
       print('  converted successfully from '+oldEncoding+': '+newPath)
 
