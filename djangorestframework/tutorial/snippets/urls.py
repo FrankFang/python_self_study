22ce1a1f9ba30eeea2e04f8f9dfb716be47b4f12
Frank Fang <frankfang1990@gmail.com>
2013-10-29 00:21:35 +0800

    Update.

--- djangorestframework/tutorial/snippets/urls.py
+++ djangorestframework/tutorial/snippets/urls.py
@@ -1,8 +1,12 @@
+from snippets import views
+
 __author__ = 'frank'
 from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('snippets.views',
-                       url(r'snippets/$', 'snippet_list'),
-                       url(r'snippets/(?P<pk>[0-9]+)/$', 'snippet_detail'),
+                       url(r'snippets/$', views.SnippetList.as_view()),
+                       url(r'snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 )
 
+urlpatterns = format_suffix_patterns(urlpatterns)
