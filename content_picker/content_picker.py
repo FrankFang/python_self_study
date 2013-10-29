bb18d3dbcefb50f5dbdd7fbb7089d4f445d6b1db
Frank Fang <frankfang1990@gmail.com>
2013-10-30 02:57:04 +0800

    debug

--- content_picker/content_picker.py
+++ content_picker/content_picker.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-from nvd3 import pieChart
+from sys import argv
 import re
 import urllib2
 
 __author__ = 'frank'
 
-page = urllib2.urlopen('http://www.cnblogs.com/seer/p/3395888.html').read() + '<link href=xxxx>'
+try:
+    url = argv[1]
+except:
+    exit('url needed')
+
+page = urllib2.urlopen(url).read() + '<link href=xxxx>'
 
 
 def convert(_page):
@@ -35,7 +40,7 @@ def wipe_html_tags(_page_in_utf8):
     _content = re.sub(r'<\s*?style.*?>(.|\n)*?</+?style.*?>', '', _content, flags=my_flag)
     _content = re.sub(r'<!--(.|\n)*?-->', '', _content, flags=my_flag)
     _content = re.sub(r'&.{1,5};|&#.{1,5};', '', _content, flags=my_flag)
-    _content = re.sub(r'<.*?>', '', _content, flags=my_flag)
+    _content = re.sub(r'<([^>]|\n)+>', '', _content, flags=my_flag)
     _content = re.sub(r'$\s*?\n', '\n', _content, flags=my_flag)
     return _content
 
@@ -47,24 +52,17 @@ lines = page_without_html_tag.split('\n')
 
 block_length_array = [len(a) + len(b) + len(c) for a, b, c in zip(lines, lines[1:], lines[2:])]
 
-#print ([(bl, lines[index]) for index, bl in enumerate(block_length_array)])
-
-#print(block_length_array)
-
 content = ''
 start = 0
 end = 0
 for index, block_length in enumerate(block_length_array):
-    #print(index, block_length)
     current_content = ''
     current_start = index
     for index2, block_length2 in enumerate(block_length_array[index:]):
-        #print('-', index2, block_length2)
         if block_length2 != 0:
-            current_content += lines[index + index2] + '\n'
+            if lines[index + index2].strip() != '':
+                current_content += lines[index + index2] + '\n'
         else:
-            #print('block_length ', block_length2)
-            #print(index2, '-', lines[index2])
             current_end = index2
             break
     if len(current_content) > len(content):
@@ -72,25 +70,4 @@ for index, block_length in enumerate(block_length_array):
         end = current_end
         content = current_content
 
-#print(start)
-#print(end)
 print(content)
-
-output_file = open('/Users/frank/test/test.html', 'w')
-
-header = 'pieChart'
-chart = pieChart(name=header, color_category='category20c', height=450, width=450)
-chart.set_containerheader("\n\n<h2>" + header + "</h2>\n\n")
-
-#Create the keys
-xdata = range(0, len(block_length_array))
-ydata = block_length_array
-
-#Add the serie
-extra_serie = {"tooltip": {"y_start": "", "y_end": " cal"}}
-chart.add_serie(y=ydata, x=xdata, extra=extra_serie)
-chart.buildhtml()
-output_file.write(chart.htmlcontent)
-
-#close Html file
-output_file.close()
